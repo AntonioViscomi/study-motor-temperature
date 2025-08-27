53d84530de7339332fd68206f417b36cbef2a6eb
AntonioViscomi <antonio.viscomi@iit.it>
2025-08-27 17:22:08 +0200

    Improved function to find overheat in a vectorized version. It should now efficiently handle large amount of data.

--- code/analyzeTemperatureData/main.m
+++ code/analyzeTemperatureData/main.m
@@ -6,7 +6,7 @@ addpath(genpath("../../"))
 experimentData = load("data/torso_pitch_mj1_amo_aksim_peak_torque_20kg_load_1744984764.949400.mat");
 
 % Specify the joint to consider:
-joint_name = "torso_pitch_mj1_real_aksim2"; 
+joint_name = "torso_pitch_mj1_real_aksim2"; % You have to know the name of the joint you want to consider.
 joint_index = getJointIndex(joint_name, experimentData);
 
 temperature = getTemperatureData(joint_name, experimentData);
@@ -14,4 +14,5 @@ timestamps = getTimestamps(experimentData);
 
 % plotTemperatureData(timestamps,temperature, joint_index, joint_name)
 
-[FOC_TDB_I2C_NACK, FOC_TDB_NO_MEAS, TDB_LOST_CONFIG, TDB_ANY_CONFIG] = diagnosticTemperature(temperature);
+temperatureHardwareLimit = 55;
+diagnostic = errorHandlingTemperature(temperature, timestamps, temperatureHardwareLimit);
