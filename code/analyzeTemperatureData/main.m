b241220c3ff2b5960c157698a01282f0c973e61a
AntonioViscomi <antonio.viscomi@iit.it>
2025-08-28 09:36:19 +0200

    Added interactive file selection

--- code/analyzeTemperatureData/main.m
+++ code/analyzeTemperatureData/main.m
@@ -2,8 +2,10 @@
 clear, clc, close all
 
 % Load data
-addpath(genpath("../../"))
-experimentData = load("data/torso_pitch_mj1_amo_aksim_peak_torque_20kg_load_1744984764.949400.mat");
+[experimentName, experimentPath] = uigetfile('\study-motor-temperature\data', '*.mat');
+experimentData = load([experimentPath experimentName]);
+
+clear experimentName experimentPath
 
 % Specify the joint to consider:
 joint_name = "torso_pitch_mj1_real_aksim2"; % You have to know the name of the joint you want to consider.
@@ -12,7 +14,7 @@ joint_index = getJointIndex(joint_name, experimentData);
 temperature = getTemperatureData(joint_name, experimentData);
 timestamps = getTimestamps(experimentData);
 
-% plotTemperatureData(timestamps,temperature, joint_index, joint_name)
+plotTemperatureData(timestamps,temperature, joint_index, joint_name)
 
 temperatureHardwareLimit = 55; % Example value
 diagnostic = errorHandlingTemperature(temperature, timestamps, temperatureHardwareLimit);
