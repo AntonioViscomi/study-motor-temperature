539ff48bfef7819dedefea2784c0dd0ce8fe701c
AntonioViscomi <antonio.viscomi@iit.it>
2025-09-23 12:31:42 +0200

    Trying to run tests in headless mode

--- code/analyzeTemperatureData/tests/test_detectOverheating.m
+++ code/analyzeTemperatureData/tests/test_detectOverheating.m
@@ -24,34 +24,9 @@ function test_overheatingSynthetic(testCase)
   verifyEqual(testCase, length(R.endIdx), 2)
   % Visualize the information for visual check.
   figure;
-  plotOverheatingZones(t, T, mask, R, thr)
+  plotOverheatingZones(t, T, mask, R)
     
 end
-
-% function test_overheatingReal(testCase)
-% 
-%     [experimentName, experimentPath] = uigetfile({'*.mat','Data Files (*.mat)'}, ...
-%                                                   'Select the real data to run the test.');
-% 
-%     experimentData = load([experimentPath experimentName]);
-% 
-%     joint_name  = "torso_pitch_mj1_real_aksim2";
-% 
-%     T = getTemperatureData(joint_name, experimentData);
-%     t  = getTimestamps(experimentData);
-% 
-%     thr = 55;
-% 
-%     [mask, R] = detectOverheating(T, t, thr);
-% 
-%     verifyEqual(testCase, length(R.startIdx), 1)
-%     verifyEqual(testCase, length(R.endIdx), 1)
-% 
-%     % Visualize the information for visual check.
-%     figure;
-%     plotOverheatingZones(T, t, mask, R, thr)
-% 
-% end
 
 % --- helper assertion ----------------------------------------------------
 function verifyThatWithinTolerance(tc, actual, expected, tol)
